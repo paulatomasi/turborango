48329afbf184dc28f566bd50f1bb1d175b5d4480
Paula Tomasi <paulatomasi@outlook.com>
2015-06-10 15:05:28 -0300

    feat(TurboRango)

---
+++ src/TurboRango/TurboRango.Web/Controllers/SorteiosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TurboRango.Web.Controllers
+{
+    public class SorteiosController : Controller
+    {
+        // GET: Sorteios
+        public ActionResult Index()
+        {
+            ViewBag.QtdRestaurantes = 33;
+            return View();
+        }
+    }
+}
