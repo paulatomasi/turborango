eebf274b0c18066502aff173091dc16cb1ffd331
Paula Tomasi <paulatomasi@outlook.com>
2015-06-11 16:14:50 -0300

    feat(TurboRango)

---
+++ src/TurboRango/TurboRango.Dominio/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace TurboRango.Dominio.Utils
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<T>(this T value)
+        {
+            var type = typeof(T);
+            var memInfo = type.GetMember(value.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return ((DescriptionAttribute)attributes[0]).Description;
+        }
+    }
+}
