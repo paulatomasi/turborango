dd7f57bfe0e99a3d64147bc9d2717ebc767980e7
Paula Tomasi <paulatomasi@outlook.com>
2015-06-12 00:07:37 -0300

    feat(TurboRango) Avaliação CRUD

--- src/TurboRango/TurboRango.Dominio/Restaurante.cs
+++ src/TurboRango/TurboRango.Dominio/Restaurante.cs
@@ -4,16 +4,6 @@ namespace TurboRango.Dominio
 {
     public class Restaurante : Entidade
     {
-        #region anotacoes
-        /* private Contato contato
-         * internal contato
-         * {
-         *  get { return this.contato }
-         *  set { this.contato = value }
-         * }
-         */
-        #endregion
-
         public int? Capacidade { get; set; }
         public string Nome { get; set; }
         public Categoria Categoria { get; set; }
